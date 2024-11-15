718d62b76e65238796c9eb835fccefa9e69db86b
Charles Kubicek <charles.kubicek@springernature.com>
2024-11-15 22:14:37 +0000

    Add the midi clip cycler

--- abletonosc/custom.py
+++ abletonosc/custom.py
@@ -1,14 +1,171 @@
 from functools import partial
+from _Framework.ControlSurfaceComponent import ControlSurfaceComponent
+from _Framework.ControlSurface import ControlSurface
 import Live
 import logging
 from typing import Optional, Tuple, Any
 from .handler import AbletonOSCHandler
+import time
+import re
+
+class ClipOps(AbletonOSCHandler):
+
+    def __init__(self, manager):
+        super().__init__(manager)
+        self._manager = manager
+
+        ## TODO implement tripples as well as bars
+        self._note_shift_quantization = 0.25
+
+    def create_clip_and_notes(self, notes, title, length=4):
+        '''
+        Create a clip with the given notes and title.
+
+        If its in the arrange view, add the notes to the selected clip.
+
+        :param notes:
+        :param title:
+        :return:
+        '''
+
+        clip = self.get_or_create_selected_clip(length)
+        if clip is not None:
+            clip.loop_end = length  # a clip from arrangement might be different
+            clip.remove_notes_extended(from_time=0, from_pitch=0, time_span=clip.loop_end, pitch_span=128)
+
+            clip.add_new_notes(tuple(notes))
+            clip.name = title
+
+
+    def get_or_create_selected_clip(self, length, create=True, remove_existing=True):
+        vw = self.application.view.focused_document_view
+        # self.log("focused_document_view: " + str(vw))
+
+        if vw == 'Arranger':
+
+            tm = self.song.current_song_time
+            # self.log_message("current_song_time: " + str(tm))
+
+            track = self.song.view.selected_track
+            for clip in track.arrangement_clips:
+                # self.log(f"is audio clip {clip.is_audio_clip})")
+                # self.log(f"is audio clip {clip.is_midi_clip})")
+                # self.log(f"start time {clip.start_time})")
+                # self.log(f"start marker {clip.start_marker})")
+                #
+                # self.log(f"end time {clip.end_time})")
+                # self.log(f"end marker {clip.loop_end})")
+                # self.log(f"end time {str(clip)}")
+                # self.log(f"loop end {clip.loop_end})")
+                # self.log(f"end time {str(clip)}")
+
+                if clip.is_midi_clip and clip.start_time <= tm < clip.end_time:
+                    return clip
+
+            # self.create_clip_and_copy_it_to_arrangement(track, Patterns.c_line, tm)
+
+
+        elif not self.song.view.highlighted_clip_slot.has_clip and create:
+            self.song.view.highlighted_clip_slot.create_clip(float(length))
+            clip = self.song.view.highlighted_clip_slot.clip
+
+            return clip
+        elif self.song.view.highlighted_clip_slot.has_clip:
+            clip = self.song.view.highlighted_clip_slot.clip
+
+            if remove_existing:
+                clip.remove_notes_extended(from_time=0, from_pitch=0, time_span=clip.loop_end, pitch_span=128)
+
+            return clip
+
+        return None
+
+
+class PatternCycler(AbletonOSCHandler):
+    def __init__(self, ins, patterns, clip_ops):
+        super().__init__(ins)
+        self._control_surface = ins
+        self._patterns = patterns
+        self._clip_ops = clip_ops
+
+        self.counter = 0
+        self.last_press = int(time.time())
+
+    def was_used_within_window(self):
+        return int(time.time()) - self.last_press < 4
+
+    def next(self):
+        if not self.was_used_within_window():
+            self.counter = 0
+
+        name, notes = self._patterns[self.counter]
+        self._clip_ops.create_clip_and_notes(notes, name)
+        self.counter = (self.counter + 1) % len(self._patterns)
+
+        self.last_press = time.time()
+
+
+class Patterns(object):
+    c_line = [Live.Clip.MidiNoteSpecification(pitch=60, start_time=0, duration=4, velocity=127)]
+    g_line = [Live.Clip.MidiNoteSpecification(pitch=55, start_time=0, duration=4, velocity=127)]
+    notes_c_kicks = [
+        Live.Clip.MidiNoteSpecification(pitch=60, start_time=0, duration=1, velocity=127),
+        Live.Clip.MidiNoteSpecification(pitch=60, start_time=1, duration=1, velocity=127),
+        Live.Clip.MidiNoteSpecification(pitch=60, start_time=2, duration=1, velocity=127),
+        Live.Clip.MidiNoteSpecification(pitch=60, start_time=3, duration=1, velocity=127)
+    ]
+
+    notes_off_beat = [
+        Live.Clip.MidiNoteSpecification(pitch=60, start_time=0.5, duration=0.5, velocity=127),
+        Live.Clip.MidiNoteSpecification(pitch=60, start_time=1.5, duration=0.5, velocity=127),
+        Live.Clip.MidiNoteSpecification(pitch=60, start_time=2.5, duration=0.5, velocity=127),
+        Live.Clip.MidiNoteSpecification(pitch=60, start_time=3.5, duration=0.5, velocity=127)
+    ]
+
+    # midi_drum_loops = [
+    #     (build_loop([[0,0,9,0],[0,0,9,3],[0,0,0,0],[0,9,3,0]]))
+    # ]
+
+    notes_c_16s = [
+        Live.Clip.MidiNoteSpecification(pitch=60, start_time=0, duration=0.25, velocity=75),
+        Live.Clip.MidiNoteSpecification(pitch=60, start_time=0.25, duration=0.25, velocity=100),
+        Live.Clip.MidiNoteSpecification(pitch=60, start_time=0.5, duration=0.25, velocity=127),
+        Live.Clip.MidiNoteSpecification(pitch=60, start_time=0.75, duration=0.25, velocity=100),
+        Live.Clip.MidiNoteSpecification(pitch=60, start_time=1, duration=0.25, velocity=75),
+        Live.Clip.MidiNoteSpecification(pitch=60, start_time=1.25, duration=0.25, velocity=100),
+        Live.Clip.MidiNoteSpecification(pitch=60, start_time=1.5, duration=0.25, velocity=127),
+        Live.Clip.MidiNoteSpecification(pitch=60, start_time=1.75, duration=0.25, velocity=100),
+        Live.Clip.MidiNoteSpecification(pitch=60, start_time=2, duration=0.25, velocity=75),
+        Live.Clip.MidiNoteSpecification(pitch=60, start_time=2.25, duration=0.25, velocity=100),
+        Live.Clip.MidiNoteSpecification(pitch=60, start_time=2.5, duration=0.25, velocity=127),
+        Live.Clip.MidiNoteSpecification(pitch=60, start_time=2.75, duration=0.25, velocity=100),
+        Live.Clip.MidiNoteSpecification(pitch=60, start_time=3, duration=0.25, velocity=75),
+        Live.Clip.MidiNoteSpecification(pitch=60, start_time=3.25, duration=0.25, velocity=100),
+        Live.Clip.MidiNoteSpecification(pitch=60, start_time=3.5, duration=0.25, velocity=127),
+        Live.Clip.MidiNoteSpecification(pitch=60, start_time=3.75, duration=0.25, velocity=100)
+    ]
+
+    basic_midi_cycles = [
+        ('C Line', c_line),
+        ('C Beats', notes_c_kicks),
+        ('C Offbeat', notes_off_beat),
+        ('C 16s', notes_c_16s),
+        ('G Line', g_line),
+    ]
 
 
 class CustomHandler(AbletonOSCHandler):
     def __init__(self, manager):
         super().__init__(manager)
+        self.logger = logging.getLogger("abletonosc")
         self.class_identifier = "custom"
+        self.clip_ops = ClipOps(self)
+        self.midi_pattern_cycler = PatternCycler(self, Patterns.basic_midi_cycles, self.clip_ops)
+
+        self.name_guesser = NameGuesser(self, self.song)
+
+    def log_message(self, message):
+        self.logger.info(message)
 
     def init_api(self):
 
@@ -116,6 +273,7 @@ class CustomHandler(AbletonOSCHandler):
             while selected_device() != next and check < 100:
                 _scroll_selected_device_chain(NavDirection.left)
                 check += 1
+
 
         def device_nav_right_ignoring_inner_devices(params):
             NavDirection = Live.Application.Application.View.NavDirection
@@ -468,6 +626,9 @@ class CustomHandler(AbletonOSCHandler):
             if next_index < all_tracks:
                 self.song.view.selected_track = self.song.tracks[next_index]
 
+            ### Do some events afterwards as we don't listen for events
+            self.name_guesser.update_track_names()
+
         def track_nav_dec(param):
             selected_track = self.song.view.selected_track  # Get the currently selected track
 
@@ -478,6 +639,9 @@ class CustomHandler(AbletonOSCHandler):
 
             if next_index >= 0:
                 self.song.view.selected_track = self.song.tracks[next_index]
+
+            ### Do some events afterwards as we don't listen for events
+            self.name_guesser.update_track_names()
 
         def _scroll_device_chain(direction):
             view = self.application.view
@@ -492,6 +656,12 @@ class CustomHandler(AbletonOSCHandler):
             logger.info(f"fire_scene, params {self.song.scenes}")
             self.song.scenes[int(params[0])].fire()
             name_clips_to_scene_number_and_clip_length(params)
+
+        def iterate_perc_pattern(params):
+            self.midi_pattern_cycler.next()
+
+        def update_track_names(params):
+            self.name_guesser.update_track_names()
 
         def name_clips_to_scene_number_and_clip_length(params):
             for track in self.song.tracks:  # for each track
@@ -513,6 +683,7 @@ class CustomHandler(AbletonOSCHandler):
                         if len(parts) > 1:
                             name = parts[1]
                             clip.name = f"{scene_number+1} {name} ({loop_length})"
+
 
         self.osc_server.add_handler("/live/custom/fire/scene", fire_scene)
         self.osc_server.add_handler("/live/custom/set/toggle_inst_utility", toggle_instrument_utility)
@@ -559,4 +730,127 @@ class CustomHandler(AbletonOSCHandler):
         # self.osc_server.add_handler("/live/custom/toggle_mono", toggle_mono_on_master)
         self.osc_server.add_handler("/live/custom/toggle_low_pass", toggle_low_pass_on_master)
         self.osc_server.add_handler("/live/custom/toggle_high_pass", toggle_high_pass_on_master)
+
+        self.osc_server.add_handler("/live/custom/next_midi_pattern", iterate_perc_pattern)
+
         self.osc_server.add_handler("/live/view/message", show_message)
+
+
+class NameGuesser(AbletonOSCHandler):
+
+    def __init__(self, ins, song):
+        super().__init__(ins)
+        self._manager = ins
+        self.logger = logging.getLogger("abletonosc")
+        self._song = song
+        self.default_track_colour_index = 69  # dark grey
+
+    def log_message(self, message):
+        self.logger.info(message)
+
+    def get_name_of_instrument_or_sample(self, track):
+        for d in track.devices:
+            name, colour = self.get_name_of_instrument_or_sample_for_device(d)
+            if name is not None:
+                return name, colour
+
+        return None, None
+
+    def get_name_of_instrument_or_sample_for_device(self, d):
+        found_name = None
+        colour = None
+        if str(d.type) == 'instrument':
+            device_name = d.name
+            class_name = d.class_name
+            self.log_message("device class_name: " + str(d.class_name))
+            self.log_message("device class_display_name: " + str(d.class_display_name))
+            self.log_message(f"device name: {device_name}")
+
+            if class_name == 'PluginDevice':
+                found_name = device_name
+                colour = 25
+            if class_name == 'OriginalSimpler':
+                found_name = device_name
+                colour = 33
+            elif device_name == 'Instrument Rack':
+                name, c = self.find_rack_chain_names(d)
+                if name is not None:
+                    found_name = name
+                    colour = c
+
+            self.log_message(f"found_name: {found_name}")
+
+            if found_name is not None:
+                found_name = (found_name
+                    .removeprefix("CK ")
+                    .removeprefix("INST ")
+                    .removeprefix("AYNIL "))
+
+                return re.sub(r'[^a-zA-Z0-9]', '', found_name), colour
+
+        return None, None
+
+    def find_rack_chain_names(self, d):
+        if d.can_have_chains:  # it's a rack #) == 'MidiEffectGroupDevice':
+            self.log_message("d.chanins len: " + str(len(d.chains)))
+            for chain in d.chains:
+                for c_d in chain.devices:
+                    name, colour = self.get_name_of_instrument_or_sample_for_device(c_d)
+                    if name is not None:
+                        return name, colour
+
+        return None, None
+
+    def update_track_names(self):
+
+        not_updating = []
+
+        for track in self._song.tracks:
+            self.log_message(track.name)
+            self.log_message(track.is_grouped)
+            self.log_message(track.group_track)
+
+        for track in self._song.tracks:
+            try:
+                self.log_message(f"------------------------------------------------")
+                self.log_message(f"[{track.name}]")
+
+
+                if self.is_track_grouping_other_tracks(track):
+                    self.set_grouping_track_colour_from_others(track)
+
+                if track.name.endswith('.') \
+                        or self.is_track_grouping_other_tracks(track) \
+                        or self.track_has_already_been_updated(track) \
+                        or len(track.name) > 2:
+                    self.log_message(f"[{track.name}] Skipping")
+                    continue
+
+                if track.name.startswith('[bip]'):
+                    track.name = f"# {track.name[len('[bip] '):]} [bip]"
+                    continue
+
+                name_guess, colour = self.get_name_of_instrument_or_sample(track)
+                self.log_message(f"[{track.name}] track name guess is {str(name_guess)}")
+
+                track.name = name_guess
+                track.color_index = colour
+
+            except Exception as e:
+                self.log_message(f"Exception: {str(e)}")
+                not_updating.append(track.name)
+
+    def is_track_grouping_other_tracks(self, the_track):
+        for track in self.song.tracks:
+            if track.group_track is not None and track.group_track.name == the_track.name:
+                return True
+
+        return False
+
+    def set_grouping_track_colour_from_others(self, the_group_track):
+        track_colors = [t.color_index for t in self.song.tracks if
+                        t.group_track is not None and t.group_track.name == the_group_track.name]
+        the_group_track.color_index = sorted(list(set(track_colors)))[0]
+
+    def track_has_already_been_updated(self, the_track):
+        return the_track.color_index != self.default_track_colour_index
